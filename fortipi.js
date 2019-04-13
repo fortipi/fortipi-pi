973321961537fe9a2b3e057a5a3d34c686a6ced5
Mahgo2k <45212210+Mahgo2k@users.noreply.github.com>
2019-04-12 21:59:18 -0400

    Fixed async issues

--- fortipi.js
+++ fortipi.js
@@ -10,38 +10,34 @@ var server = app.listen(port, hostname, function() {
   console.log('Server running at http://'+ hostname + ':' + port + '/');
 });
 
-const sqlite3 = require('sqlite3').verbose();
-	
+var db = require('sqlite-sync');
+
 const path = require('path');
 const dbPath = path.resolve(__dirname, 'fortipi.db');
-const db = new sqlite3.Database(dbPath);
+db.connect(dbPath);
 
+//Gets all four data values when /dashboard is accessed
 app.get('/dashboard', function (req, res)
 {
   var temp = getMostRecentRecordData(1, 'Temperature', 'TEMPERATURE', 'TEMP_STATUS', 'F');
   var humidity = getMostRecentRecordData(2, 'Humidity', 'HUMIDITY', 'HUM_STATUS', '%');
   var pressure = getMostRecentRecordData(3, 'Pressure', 'BAROMETRIC_PRESSURE', 'BAR_STATUS', 'mbp');
   var light = getMostRecentRecordData(4, 'Light Intensity', 'LIGHT_INTENSITY', 'LIGHT_STATUS', 'lm');
-  console.log(temp);
-  return res.json([temp, humidity, pressure, light]);
+  res.json([temp, humidity, pressure, light]);
 });
 
-function getMostRecentRecordData(categoryId, category, valueColumn, statusColumn, measureUnit, callback)
+//Gets a data value and its status from the most recent recording and returns data in a JSON
+function getMostRecentRecordData(categoryId, category, valueColumn, statusColumn, measureUnit)
 {
-  let sql = 'SELECT MAX(TIMESTAMP), ' + valueColumn + ' value, ' + statusColumn + ' statusColor FROM T_ENV_HIST';
-  db.each(sql, (err, row) => {
-    if (err) {
-      throw err;
-    }
-	var record = {
-      id: categoryId,
-	  reading: category,
-	  current_value: row.value,
-	  unit_of_measure: measureUnit,
-	  status_color: row.statusColor
-    };
-	JSON.stringify(record);
-	console.log(record);
-	return record;
-  });
-}	
+  let sql = 'SELECT MAX(TIMESTAMP), ' + valueColumn + ' value, ' + statusColumn + ' statusColor FROM T_ENV_HIST'
+  var rows = db.run(sql); //'rows' since the data will output as an array, even if only one record is selected
+  var record = {
+    id: categoryId,
+	reading: category,
+	current_value: rows[0].value,
+	unit_of_measure: measureUnit,
+	status_color: rows[0].statusColor
+   };
+  JSON.stringify(record);
+  return (record);  
+}
